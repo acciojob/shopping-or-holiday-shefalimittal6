d654e3b7c8671d8ba26b11ba8a4f55cbc3d2174f
Shefali <mittal.shefali2000@gmail.com>
2025-01-12 00:30:59 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 //your code here
+// JavaScript to log the lists with data-ns-test attributes
+window.onload = function() {
+    // Accessing the shopping list items
+    const shoppingItems = document.querySelectorAll('li[data-ns-test="shoppingList"]');
+    shoppingItems.forEach(item => {
+        console.log("Shopping Item:", item.textContent);
+    });
+
+    // Accessing the holiday list items
+    const holidayItems = document.querySelectorAll('li[data-ns-test="holidayList"]');
+    holidayItems.forEach(item => {
+        console.log("Holiday Destination:", item.textContent);
+    });
+}
